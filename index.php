ed93bf4f4a42408c95356d7df4c5f35fc812b297
Andi Tangguh <anditangguhf@gmail.com>
2020-08-16 15:43:59 +0700

    Update kipi

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php include 'templates/header.php'; ?>
 
-<div class="home-banner text-center">
-    <img src="/assets/img/logo.png" alt="Banner"/>
+<div class="row">
+    <div class="col">
+        <div class="home-banner text-center">
+            <img src="/assets/img/logo.png" alt="Banner"/>
+        </div>
+    </div>
 </div>
-<div class="card-body text-center">
-    <p class="card-text">MullBank merupakan ...</p>
-    <p class="card-text">LET'S START TO<br/>MAKE CHANGE</p>
-    <button type="button" class="btn btn-primary sign-up" data-toggle="modal" data-target="#modal">Join w/ Us</button>
+<div class="row">
+    <div class="col text-center">
+        <p class="card-text">MullBank merupakan Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p class="card-text">LET'S START TO<br/>MAKE CHANGE</p>
+        <button type="button" class="btn btn-primary sign-up" data-toggle="modal" data-target="#modal">Join w/ Us</button>
+    </div>
 </div>
 
 <!-- Modal dummies -->
