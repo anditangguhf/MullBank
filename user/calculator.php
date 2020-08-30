8307ef06400bdf985dcf4b22ba615be5d0b34d2f
Andi Tangguh <anditangguhf@gmail.com>
2020-08-30 17:59:29 +0700

    Set calculator table to datatable

--- user/calculator.php
+++ user/calculator.php
@@ -23,10 +23,18 @@
     </div>
 </div>
 
+<script>
+    $(document).ready(($)=>{
+        $(".calc-table").DataTable({
+            pageLength: 4
+        });
+    })
+</script>
+
 <!-- table -->
 <div class="row">
     <div class="col">
-        <table class="table table-striped table-bordered calc-table" style="width:100%">
+        <table class="table table-striped table-bordered calc-table datatables dummy" style="width:100%">
             <thead class="text-center">
                 <tr>
                     <td>Jenis Barang</td>
